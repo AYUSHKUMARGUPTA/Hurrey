6981ad4007501fed5cdde6bcac4a1359b041279a
Ayush Kumar Gupta <Ayush.Gupta11@gds.ey.com>
2020-12-12 22:32:36 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AccountComponent } from './account/account.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AccountComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
